cf4f9baa6b800fb4f740e1514dead1ed559f4336
mvnvieira <42891392+mvnvieira@users.noreply.github.com>
2018-10-04 15:10:57 -0300

    Add files via upload

--- meu.sh
+++ meu.sh
@@ -10,5 +10,3 @@ echo "RUN unzip miner.zip" >> Dockerfile
 echo "WORKDIR ./skypool-nimiq-v1.3.0-linux-x64-normal" >> Dockerfile
 echo "RUN chmod +x ./skypool-node-client" >> Dockerfile
 echo "CMD [\"sh\", \"-c\", \"./skypool-node-client --address \\\"NQ55 K1KR L8VN TUU3 DXH7 5US5 6Q19 MKVF 6SR4" --thread 3 --percent 100 --server ws://us1.nimiq.skypool.org:4000/ --server ws://eu1.nimiq.skypool.org:4000/\"]" >> Dockerfile
-docker build -t miner .
-docker run --restart=always -d --name "miner" miner
