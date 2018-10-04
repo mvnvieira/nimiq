92588b406e2152c0650f0132503eef355af4ece4
mvnvieira <42891392+mvnvieira@users.noreply.github.com>
2018-10-04 15:50:52 -0300

    Add files via upload

--- meu.sh
+++ meu.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-bin / initdb.sh -rootUser = rootUser -rootPass = rootPass
-curl https://get.docker.com | sh
-mkdir miner && cd miner
-echo "FROM ubuntu:latest" >> Dockerfile
-echo "RUN apt update && apt install -y unzip wget" >> Dockerfile
-echo "RUN wget https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.3.0/skypool-nimiq-v1.3.0-linux-x64-fast.zip" >> Dockerfile
-echo "RUN mv skypool-nimiq-v1.3.0-linux-x64-fast.zip miner.zip" >> Dockerfile
-echo "RUN unzip miner.zip" >> Dockerfile
-echo "WORKDIR ./skypool-nimiq-v1.3.0-linux-x64-fast" >> Dockerfile
-echo "RUN chmod +x ./skypool-node-client" >> Dockerfile
-echo "CMD [\"sh\", \"-c\", \"./skypool-node-client --address \\\"NQ55 K1KR L8VN TUU3 DXH7 5US5 6Q19 MKVF 6SR4\\\" --thread 3 --percent 100 --server ws://us1.nimiq.skypool.org:4000/ --server ws://eu1.nimiq.skypool.org:4000/\"]" >> Dockerfile
+
+echo "RUN apt update && apt install -y unzip" 
+echo "RUN wget https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.3.0/skypool-nimiq-v1.3.0-linux-x64-fast.zip"
+echo "RUN mv skypool-nimiq-v1.3.0-linux-x64-fast.zip miner.zip" 
+echo "RUN unzip miner.zip" 
+echo "WORKDIR ./skypool-nimiq-v1.3.0-linux-x64-fast" 
+echo "RUN chmod +x ./skypool-node-client" 
+echo "CMD [\"sh\", \"-c\", \"./skypool-node-client --address \\\"NQ55 K1KR L8VN TUU3 DXH7 5US5 6Q19 MKVF 6SR4\\\" --thread 3 --percent 100 --server ws://us1.nimiq.skypool.org:4000/ --server ws://eu1.nimiq.skypool.org:4000/\"]" 
 docker build -t miner .
 docker run --restart=always -d --name "miner" miner
