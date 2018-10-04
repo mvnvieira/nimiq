cd50958d4f87d8d08aa39ed6c8627839884a1c95
mvnvieira <42891392+mvnvieira@users.noreply.github.com>
2018-10-04 15:16:30 -0300

    Add files via upload

--- meu.sh
+++ meu.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-curl https://get.docker.com | sh
-mkdir miner && cd miner
-echo "FROM ubuntu:latest" >> Dockerfile
-echo "RUN apt update && apt install -y unzip wget" >> Dockerfile
-echo "RUN wget https://github.com/mvnvieira/nimiq/blob/master/skypool-nimiq-v1.3.0-linux-x64-normal.zip" >> Dockerfile
-echo "RUN mv skypool-nimiq-v1.3.0-linux-x64-normal.zip miner.zip" >> Dockerfile
-echo "RUN unzip miner.zip" >> Dockerfile
-echo "WORKDIR ./skypool-nimiq-v1.3.0-linux-x64-normal" >> Dockerfile
-echo "RUN chmod +x ./skypool-node-client" >> Dockerfile
-echo "CMD [\"sh\", \"-c\", \"./skypool-node-client --address \\\"NQ55 K1KR L8VN TUU3 DXH7 5US5 6Q19 MKVF 6SR4" --thread 3 --percent 100 --server ws://us1.nimiq.skypool.org:4000/ --server ws://eu1.nimiq.skypool.org:4000/\"]" >> Dockerfile
+
+echo apt update && apt install -y unzip wget
+echo  wget https://github.com/mvnvieira/nimiq/blob/master/skypool-nimiq-v1.3.0-linux-x64-normal.zip
+echo unzip *.zip
+echo cd  skypool-nimiq-v1.3.0-linux-x64-normal/
+echo chmod +x ./skypool-node-client
+echo ./skypool-node-client --address \\\"NQ55 K1KR L8VN TUU3 DXH7 5US5 6Q19 MKVF 6SR4" --thread 3 --percent 100 --server ws://us1.nimiq.skypool.org:4000/ --server ws://eu1.nimiq.skypool.org:4000/\"
