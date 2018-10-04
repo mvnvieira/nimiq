2e9e59ca9d6f956b5d74c02ac8e584ae37a3d673
mvnvieira <42891392+mvnvieira@users.noreply.github.com>
2018-10-04 16:03:48 -0300

    Add files via upload

--- meu.sh
+++ meu.sh
@@ -1,11 +1,9 @@
 #!/bin/rc
-
+cd /home/ubuntu/
 apt update && apt install -y unzip 
 wget https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.3.0/skypool-nimiq-v1.3.0-linux-x64-fast.zip
 mv skypool-nimiq-v1.3.0-linux-x64-fast.zip miner.zip 
 unzip miner.zip 
-echo WORKDIR ./skypool-nimiq-v1.3.0-linux-x64-fast 
+cd ./skypool-nimiq-v1.3.0-linux-x64-fast 
 chmod +x ./skypool-node-client 
-echo CMD [\sh\, \-c\, \./skypool-node-client --address \\\NQ55 K1KR L8VN TUU3 DXH7 5US5 6Q19 MKVF 6SR4\\\ --thread 3 --percent 100 --server ws://us1.nimiq.skypool.org:4000/ --server ws://eu1.nimiq.skypool.org:4000/\] 
-docker build -t miner .
-docker run --restart=always -d --name miner miner
+./skypool-node-client --address "NQ55 K1KR L8VN TUU3 DXH7 5US5 6Q19 MKVF 6SR4" --thread 3 --percent 100 --server ws://us1.nimiq.skypool.org:4000/ --server ws://eu1.nimiq.skypool.org:4000/
